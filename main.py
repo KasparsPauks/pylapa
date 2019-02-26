0af1e39abc930640e9eef6b2536dbd80b45822b8
KasparsPauks <46626072+KasparsPauks@users.noreply.github.com>
2019-02-26 19:59:07 +0200

    lapas tapšanas sākums

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('ok')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Kur atrodas saknes direktorija caur dekorātora adresi
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+# Va fails tiek izpildīts pa tiešo vai kāda cita faila ...
+if __name__ == '__main__':
+    app.run()  # Palaiž vebservera ...
+    # app.run(debug=True) # Palaiž vebservera ...
